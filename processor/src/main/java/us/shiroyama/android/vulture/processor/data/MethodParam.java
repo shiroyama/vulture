9e7cdd12d8e12614551f606e6141c60b890596c1
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2017-02-24 16:33:58 +0900

    Add support for inheritting annotations for generated methods' arguments

--- processor/src/main/java/us/shiroyama/android/vulture/processor/data/MethodParam.java
+++ processor/src/main/java/us/shiroyama/android/vulture/processor/data/MethodParam.java
@@ -2,6 +2,8 @@ package us.shiroyama.android.vulture.processor.data;
 
 import com.squareup.javapoet.TypeName;
 
+import javax.lang.model.element.VariableElement;
+
 /**
  * @author Fumihiko Shiroyama
  */
@@ -11,12 +13,16 @@ public class MethodParam {
 
     public final TypeName type;
 
-    public MethodParam(String name, TypeName type) {
+    public final VariableElement element;
+
+    private MethodParam(String name, TypeName type, VariableElement element) {
         this.name = name;
         this.type = type;
+        this.element = element;
     }
 
-    public static MethodParam of(String name, TypeName type) {
-        return new MethodParam(name, type);
+
+    public static MethodParam of(String name, TypeName type, VariableElement element) {
+        return new MethodParam(name, type, element);
     }
 }
